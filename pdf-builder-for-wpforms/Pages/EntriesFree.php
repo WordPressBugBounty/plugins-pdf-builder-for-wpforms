f6442f464207cd44ffb795e12eb838b1595d1589
Sean Murphy <sean@iamseanmurphy.com>
2025-03-24 21:17:12 +0000

    1.2.126

--- pdf-builder-for-wpforms/Pages/EntriesFree.php
+++ pdf-builder-for-wpforms/Pages/EntriesFree.php
@@ -10,6 +10,6 @@ $loader=$rninstance;
     <img src="<?php echo $loader->URL?>images/fullentries.png"/>
     <div style="position: absolute;top:0;left:0;width:100%;height: 100%;background-color: black;opacity: .5;"></div>
     <div style="position: absolute;top:0;left:0;width:100%;height:100%;display: flex;align-items: center;justify-content: center;">
-        <h1 style="color:white;"><?php echo __("The entries screen is only available in the full version.","rnpdfbuilder")?> <a target="_blank" style="color:white;" href="<?php echo $loader->GetPurchaseURL() ?>">Get it here</a> </h1>
+        <h1 style="color:white;"><?php echo __("The entries screen is only available in the full version.","rnpdfbuilder")?> <a target="_blank" style="color:white;" href="<?php echo $loader->GetPurchaseURL() ?>"><?php echo __("Get it here","rnpdfbuilder")?></a> </h1>
     </div>
 </div>
